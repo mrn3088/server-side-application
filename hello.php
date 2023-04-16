d5f899f9021902063120f03ac91dce14878a6c38
Ruinan Ma <r7ma3088@gmail.com>
2023-04-16 07:35:16 +0000

    updated

---
+++ hello.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hello PHP</title>
+</head>
+<body>
+    <h1>PHP Lives!</h1>
+    <?php
+      phpinfo();
+     ?>
+    
+</body>
+</html>
