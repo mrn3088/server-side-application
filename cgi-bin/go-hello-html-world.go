b4a9e32395d5d03d1897e0a4bcd6163ceeb198bd
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-04-26 15:21:54 -0700

    go fixed

--- cgi-bin/go-hello-html-world.go
+++ cgi-bin/go-hello-html-world.go
@@ -1,37 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/cgi"
-	"time"
+    "fmt"
+    "os"
+    "time"
 )
 
 func main() {
-	err := cgi.Serve(http.HandlerFunc(handler))
-	if err != nil {
-		fmt.Println("Error serving CGI:", err)
-	}
-}
+    fmt.Println("Cache-Control: no-cache")
+    fmt.Println("Content-type: text/html\n")
+    fmt.Println("<html>")
+    fmt.Println("<head>")
+    fmt.Println("<title>Hello, Go!</title>")
+    fmt.Println("</head>")
+    fmt.Println("<body>")
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Print("Cache-Control: no-cache\n")
-	fmt.Print("Content-type: text/html\n\n")
-	fmt.Print("<html>")
-	fmt.Print("<head>")
-	fmt.Print("<title>Hello, Go!</title>")
-	fmt.Print("</head>")
-	fmt.Print("<body>")
+    fmt.Println("<h1>Monster Team was here - Hello, Go!</h1>")
+    fmt.Println("<p>This page was generated with the Go programming langauge</p>")
 
-	fmt.Print("<h1>Monster Team was here - Hello, Go!</h1>")
-	fmt.Print("<p>This page was generated with the Go programming language</p>")
+    date := time.Now().Local()
+    fmt.Println("<p>Current Time: ", date, "</p>")
 
-	date := time.Now().Format(time.RFC1123)
-	fmt.Printf("<p>Current Time: %s</p>", date)
+    // IP Address is an environment variable when using CGI
+    address := os.Getenv("REMOTE_ADDR")
+    fmt.Println("<p>Your IP Address: ", address, "</p>")
 
-	address := r.RemoteAddr
-	fmt.Printf("<p>Your IP Address: %s</p>", address)
-
-	fmt.Print("</body>")
-	fmt.Print("</html>")
+    fmt.Println("</body>")
+    fmt.Println("</html>")
 }
