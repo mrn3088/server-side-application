e3d59a9b0029656aa137112ef479057547837cee
Ruinan Ma <r7ma3088@gmail.com>
2023-04-26 04:48:36 +0000

    updated cgi file

--- cgi-bin/perl-html-world.pl
+++ cgi-bin/perl-html-world.pl
@@ -8,7 +8,7 @@ print "<title>Hello, Perl!</title>";
 print "</head>";
 print "<body>";
 
-print "<h1>Thomas was here - Hello, Perl!</h1>";
+print "<h1>Monster Team was here - Hello, Perl!</h1>";
 print "<p>This page was generated with the Perl programming langauge</p>";
 
 $date = localtime();
