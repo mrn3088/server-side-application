a28bdad40ce8a34dc863b811d449b2f7fc1a2b51
Jiwen Luo <jiwenluo@Jiwens-Air.attlocal.net>
2023-04-27 17:53:20 -0700

    add empty file and test hello-world.php

---
+++ cgi-bin/php-hello-html-world.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World</title>
+</head>
+<body>
+	<h1>Hello World!</h1>
+	<p>Current Date/Time: <?php echo date('Y-m-d H:i:s'); ?></p>
+	<p>Your IP address is: <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
+</body>
+</html>
