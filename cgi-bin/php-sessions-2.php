b9d5e68944250d01c153ff5507950251594f4308
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-04-30 00:56:50 -0700

    fix php session

--- cgi-bin/php-sessions-2.php
+++ cgi-bin/php-sessions-2.php
@@ -6,7 +6,7 @@
         <h1>PHP Sessions Page 2</h1>
         <?php
         session_start();
-        $_SESSION['username'] = $_POST['username'];
+        // $_SESSION['username'] = $_POST['username'];
 
         if (isset($_COOKIE['PHPSESSID'])) {
             $cookie = $_COOKIE['PHPSESSID'];
