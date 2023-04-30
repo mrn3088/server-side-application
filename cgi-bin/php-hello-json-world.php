ea589d5547cac7dc1cef154d3c62eade53052917
Jiwen Luo <jiwenluo@Jiwens-Air.attlocal.net>
2023-04-30 16:12:35 -0700

    fix php json time and ip

--- cgi-bin/php-hello-json-world.php
+++ cgi-bin/php-hello-json-world.php
@@ -3,6 +3,8 @@
     $data = array(
         'title' => 'Hello, php!',
         'heading' => 'hello, php!',
+        'date' => date('Y-m-d H:i:s'),
+        'ip' => $_SERVER['REMOTE_ADDR'],
         'message' => 'This page was generated with the PHP programming language'
     );
 
