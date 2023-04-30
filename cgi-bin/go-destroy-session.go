e5a32c2e1abc15b51cf017b8d738c7374b844981
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-04-30 14:39:14 -0700

    updated all go cgi source code

---
+++ cgi-bin/go-destroy-session.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/cgi"
+)
+
+func main() {
+	_ = cgi.Serve(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Generate the HTTP header
+		w.Header().Set("Content-Type", "text/html")
+
+		// Output HTML content
+		fmt.Fprint(w, `<html>
+<head>
+<title>Go Session Destroyed</title>
+</head>
+<body>
+<h1>Session Destroyed</h1>
+<a href="/cgi-form/go-cgiform.html">Back to the Go CGI Form</a><br />
+<a href="/cgi-bin/go-sessions-1">Back to Page 1</a><br />
+<a href="/cgi-bin/go-sessions-2">Back to Page 2</a>
+</body>
+</html>`)
+	}))
+}
---
+++ cgi-bin/go-session-2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/cgi"
+)
+
+func main() {
+	_ = cgi.Serve(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Generate the HTTP header
+		w.Header().Set("Content-Type", "text/html")
+
+		// Get the username from the URL query parameter
+		username := r.URL.Query().Get("username")
+
+		// Output HTML content
+		fmt.Fprintf(w, `<html>
+<head>
+<title>Go Sessions Page 2</title>
+</head>
+<body>
+<h1>Go Sessions Page 2</h1>
+<p><b>Username:</b> %s</p>
+<br/><br/>
+<a href="/cgi-bin/go-sessions-1?username=%s">Session Page 1</a><br/>
+<a href="/go-cgiform.html">Go CGI Form</a><br />
+<form style="margin-top:30px" action="/cgi-bin/go-destroy-session" method="get">
+<button type="submit">Destroy Session</button>
+</form>
+</body>
+</html>`, username, username)
+	}))
+}
