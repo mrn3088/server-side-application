db9f96d32710b10984bfa8c210f9acf84e52ed21
Ruinan Ma <r7ma3088@gmail.com>
2023-05-10 02:47:05 +0000

    hw3 0

---
+++ server.js
@@ -0,0 +1,17 @@
+var jsonServer = require('json-server');
+
+// Returns an Express server
+var server = jsonServer.create();
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(jsonServer.defaults());
+
+// Add custom routes
+//server.get('/posts', function (req, res) { res.json({ msg: 'hello' }) })
+
+// Returns an Express router
+var router = jsonServer.router('db.json');
+
+server.use(router);
+
+server.listen(3000);
