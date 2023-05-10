a56c3388fa17f2d2efe283bda5a765dbc280bc96
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-05-09 21:11:24 -0700

    collect static, performance

---
+++ collector/collector.js
@@ -0,0 +1,50 @@
+const collectStatic = function (collectedData) {
+    collectedData.static['userAgent'] = navigator.userAgent;
+    collectedData.static['language'] = navigator.language;
+    collectedData.static['cookie'] = navigator.cookieEnabled;
+    collectedData.static['js-enabled'] = true;
+
+    collectedData['image-enabled'] = false;
+    let img = new Image();
+    img.onload = function () {
+        collectedData['image-enabled'] = true;
+    };
+    img.src = '\images\testImage.png';
+
+    collectedData['css-enabled'] = false;
+    let tmp = document.createElement('div');
+    tmp.style.display = 'none';
+    document.body.appendChild(tmp);
+    collectedData.static['css-enabled'] = (window.getComputedStyle(tmp).display === 'none');
+    document.body.removeChild(tmp);
+
+    collectedData.static['screen-width'] = window.screen.width;
+    collectedData.static['screen-height'] = window.screen.height;
+    collectedData.static['window-width'] = window.innerWidth;
+    collectedData.static['window-height'] = window.innerHeight;
+
+    if (navigator.connection) {
+        collectedData.static['connection-type'] = navigator.connection.effectiveType;
+    }
+};
+
+const collectPerformance = function (collectedData) {
+    const observer = new PerformanceObserver((list) => {
+        list.getEntries().forEach((entry) => {
+            collectedData.performance['timing-object'] = entry.toJSON();
+        });
+    });
+
+    observer.observe({ entryTypes: ["navigation"] });
+    collectedData.performance['page-start-load'] = collectedData.performance['timing-object'].loadEventStart;
+    collectedData.performance['page-end-load'] = collectedData.performance['timing-object'].loadEventEnd;
+    collectedData.performance['total-load-time'] = collectedData.performance['timing-object'].duration;
+};
+
+window.addEventListener('load', function () {
+    let collectedData = { 'static': {}, 'performance': {}, 'activity': {} };
+    collectStatic(collectedData);
+    collectPerformance(collectedData);
+    console.log(collectedData);
+});
+
